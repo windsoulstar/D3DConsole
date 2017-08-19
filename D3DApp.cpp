2c3b0eccb43491ff311661bee4617ef97a819173
shixiaowen <289919448@qq.com>
2017-08-19 18:01:39 +0800

    常规提交

--- D3DApp.cpp
+++ D3DApp.cpp
@@ -1,5 +1,10 @@
 #include "stdafx.h"
 #include "D3DApp.h"
+#include <sstream>
+
+float D3DApp::AspectRatio() const {
+	return static_cast<float>(mClientWidth) / mClientHeight;
+}
 
 int D3DApp::Run() {
 	MSG msg = { 0 };
@@ -22,4 +27,26 @@ int D3DApp::Run() {
 		}
 	}
 	return (int)msg.wParam;
+}
+
+void D3DApp::CalculateFrameStats() {
+	static int frameCnt = 0;
+	static float timeElapsed = 0.0f;
+
+	frameCnt++;
+
+	if ((mTimer.TotalTime() - timeElapsed) >= 1.0f) {
+		float fps = (float)frameCnt;
+		float mspf = 1000.0f / fps;
+
+		std::wostringstream outs;
+		outs.precision(6);
+		outs << mMainWndCaption << L""
+			<< L"FPS:" << fps << L""
+			<< L"Frame Time" << mspf << L"(ms)";
+		SetWindowText(mhMainWnd, outs.str().c_str());
+
+		frameCnt = 0;
+		timeElapsed += 1.0f;
+	}
 }
